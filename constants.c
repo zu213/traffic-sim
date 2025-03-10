a17edcabbfc3bd3e22d3cc2d31f6ee6b6f4e2a58
Zach Upstone <zu213@bath.ac.uk>
2025-03-10 21:28:06 +0000

    Added initial constants

---
+++ constants.c
@@ -0,0 +1,13 @@
+#include "constants.h"
+#include <windows.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+const int roadWidth2 = HEIGHT / 8;
+const int halfRoadWidth2 = HEIGHT / 16;
+const int topRoadHeight2 = HEIGHT / 2 - roadWidth2 / 2;
+const int counterNumberSize2 = PADDING / 6;
+
+const int black2 = RGB(0,0,0);
+const int white2 = RGB(255,255,255);
---
+++ constants.h
@@ -0,0 +1,12 @@
+#define ENTRIES 5
+#define WIDTH 640
+#define HEIGHT 480
+#define PADDING 120
+#define CARLENGTH 20
+extern const int roadWidth2;
+extern const int halfRoadWidth2;
+extern const int topRoadHeight2;
+extern const int counterNumberSize2;
+
+extern const int black2;
+extern const int white2;
